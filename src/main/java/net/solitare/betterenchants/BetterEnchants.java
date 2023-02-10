09671467df3f8a57fe328412b83828ca48cb6793
Chris Lim <jhlim101@snu.ac.kr>
2023-02-10 17:45:34 +0900

    Initial commit

---
+++ src/main/java/net/solitare/betterenchants/BetterEnchants.java
@@ -0,0 +1,15 @@
+package net.solitare.betterenchants;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterEnchants implements ModInitializer {
+	public static final String MOD_ID = "betterenchants";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/solitare/betterenchants/BetterEnchantsClient.java
@@ -0,0 +1,10 @@
+package net.solitare.betterenchants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterEnchantsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
